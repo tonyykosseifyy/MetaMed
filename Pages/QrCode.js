bfaefa37a38003c9cdd56d5f5d97e72c124670a9
tonyy kosseifyy <tonykosseify123@gmail.com>
2022-11-12 12:45:42 +0200

    take picture button

--- Pages/QrCode.js
+++ Pages/QrCode.js
@@ -1,10 +1,37 @@
+import React from 'react'
 import AppText from '../components/AppText'
 import Background from '../components/Background'
+import { StyleSheet, View, TouchableOpacity} from 'react-native'
 
-export default function QrCode() {
+ const QrCode = () => {
   return (
    <Background>
-    <AppText style={{fontFamily: 'italic'}}>Charbel Daoud Manyak</AppText>
+		<View style={styles.container}>
+      <TouchableOpacity style={styles.touchableOpacity}>
+        <AppText>Take picture</AppText>
+        </TouchableOpacity>
+		</View>
    </Background>
   )
-}
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "red",
+  },
+  touchableOpacity: {
+    width: 130,
+    borderRadius: 4,
+    backgroundColor: '#14274e',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 40
+  }
+})
+
+export default QrCode ;
