c9a461849d61099dfd363f569a6b02598b64e80b
tonyy kosseifyy <tonykosseify123@gmail.com>
2022-11-12 13:15:16 +0200

    open camera

--- Pages/QrCode.js
+++ Pages/QrCode.js
@@ -1,27 +1,51 @@
 import React from 'react'
 import AppText from '../components/AppText'
 import Background from '../components/Background'
-import { StyleSheet, View, TouchableOpacity} from 'react-native'
+import { StyleSheet, Dimensions,  View, TouchableOpacity} from 'react-native'
+let ScreenHeight = Dimensions.get("window").height;
+
+import { Camera } from 'expo-camera'
+let camera = Camera
+
 
  const QrCode = () => {
-  return (
-   <Background>
+  const [startCamera,setStartCamera] = React.useState(false)
+  const __startCamera = async () => {
+    const {status} = await Camera.requestPermissionsAsync()
+    if(status === 'granted'){
+      setStartCamera(true) 
+   } else{
+     Alert.alert("Access denied")
+   }}
+  
+    { startCamera ? (
+      <Camera
+        style={{flex: 1,width:"100%"}}
+        ref={(r) => {
+          camera = r
+        }}
+      ></Camera>
+    ): 
+   ( <Background>
 		<View style={styles.container}>
-      <TouchableOpacity style={styles.touchableOpacity}>
+      <TouchableOpacity onPress={__startCamera} style={styles.touchableOpacity}>
         <AppText>Take picture</AppText>
-        </TouchableOpacity>
+      </TouchableOpacity>
 		</View>
    </Background>
-  )
-};
+   )
+}};
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    display: "flex",
     alignItems: 'center',
     justifyContent: 'center',
+    height: ScreenHeight,
     backgroundColor: "red",
+    
   },
   touchableOpacity: {
     width: 130,
@@ -30,8 +54,10 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    height: 40
+    height: 40,
   }
 })
 
 export default QrCode ;
+
+
