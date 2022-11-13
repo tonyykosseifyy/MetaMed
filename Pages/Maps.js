bc4f15c80010884a99d2fdb5928faf25805572a6
tonyy kosseifyy <tonykosseify123@gmail.com>
2022-11-13 17:30:42 +0200

    Werrrr shu helwee

--- Pages/Maps.js
+++ Pages/Maps.js
@@ -3,16 +3,51 @@ import MapView, { PROVIDER_GOOGLE , Marker } from "react-native-maps" ;
 import { View , Text, StyleSheet , StatusBar} from "react-native" ;
 import AppButton from "../components/AppButton";
 import MapViewDirections from 'react-native-maps-directions';
+import * as Location from 'expo-location';
 
-const origin = {latitude: 37.3318456, longitude: -122.0296002};
-const destination = {latitude: 37.771707, longitude: -122.4053769};
+
 const GOOGLE_MAPS_APIKEY = 'AIzaSyDMOei5uHcZHiLsh-BYhdYYnkIsjr9PGsI';
 
 //long 35.7212919
-//lat 34.1013648
+//lat 34.1013648  {latitude :34.098802, longitude: 35.723287 } orining {latitude :34.101935 , longitude: 35.719327 }
+
+// ,    , 
+
+const markers = [
+    {
+        latitude: 34.100934,
+        longitude: 35.728651 
+    },
+    {
+        latitude: 34.093649,
+        longitude: 35.717880 
+    },
+    {
+        latitude: 34.099654,
+        longitude: 35.713803
+    }
+];
 
 const Maps = () => {
     const [ location , setLocation ] = useState({}) ;
+    const [ destination , setDestination ] = useState({});
+    const getLocation = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            console.log("not grsnted")
+            return;
+        }
+        let userLocation = await Location.getCurrentPositionAsync({});
+        console.log("user location",userLocation)
+        setLocation({
+            latitude: userLocation.coords.latitude ,
+            longitude: userLocation.coords.longitude
+        });
+    }
+    console.log("location", location, "destination", destination);
+    useEffect(() => {
+        getLocation();
+    }, []);
 
     return (
         <View style={styles.mainView}>
@@ -25,13 +60,16 @@ const Maps = () => {
                 showsTraffic={true}
                 >
                     <MapViewDirections
-                        destination={{latitude :34.098802, longitude: 35.723287 }}
+                        destination={destination}
                         apikey={GOOGLE_MAPS_APIKEY}
-                        origin={{latitude :34.101935 , longitude: 35.719327 }}
+                        origin={ location }
                         strokeWidth={6}
                         strokeColor="red"
                     />
-                    <Marker coordinate={{latitude :34.101935 , longitude: 35.719327 }} />
+                    { markers.map((marker ,index) => (
+                        <Marker key={index} onPress={(e) => destination.latitude ? setDestination({}) : setDestination(e.nativeEvent.coordinate)} coordinate={marker} />
+                    ))}
+                    <Marker onPress={(e) => destination.latitude ? setDestination({}) : setDestination(e.nativeEvent.coordinate)} coordinate={{latitude :34.101935 , longitude: 35.719327 }} />
             </MapView>
             <Text>Working</Text>
             <AppButton onClick={() => console.log()}>Navigate to my location</AppButton>
