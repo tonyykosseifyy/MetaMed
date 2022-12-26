d8523e089e6ea102f5fd71daab86d4867e82db3b
Selim Ellieh <selimellieh@Selims-MBP.lan>
2022-12-26 17:07:08 +0200

    Api Keys are now private

--- Api/third_party.js
+++ Api/third_party.js
@@ -1,30 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_KEY = 'AIzaSyDMOei5uHcZHiLsh-BYhdYYnkIsjr9PGsI';
-const API_URL = `https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}`;
+const CLOUD_VISION_API_KEY = "API_KEY";
+const API_URL = `https://vision.googleapis.com/v1/images:annotate?key=${CLOUD_VISION_API_KEY}`;
 
 export const recognizeText = async (image) => {
-    const body = {
-      requests: [
-        {
-          image: {
-            content: image,
-          },
-          features: [
-            {
-              type: 'DOCUMENT_TEXT_DETECTION',
-              maxResults: 1,
-            },
-          ],
+  const body = {
+    requests: [
+      {
+        image: {
+          content: image,
         },
-      ],
-    };
-    
-    try{
-      const response = await axios.post(API_URL, body)
-      return response.data.responses[0].textAnnotations[0].description
-    } catch(e) {
-      console.log('Request error')
-      console.log(e)
-    }
+        features: [
+          {
+            type: "DOCUMENT_TEXT_DETECTION",
+            maxResults: 1,
+          },
+        ],
+      },
+    ],
+  };
+
+  try {
+    const response = await axios.post(API_URL, body);
+    return response.data.responses[0].textAnnotations[0].description;
+  } catch (e) {
+    console.log("Request error");
+    console.log(e);
   }
+};
--- Pages/Maps.js
+++ Pages/Maps.js
@@ -1,78 +1,88 @@
-import React, { useState , useEffect } from "react" ;
-import {MARKERS} from '../DATABASE';
-import MapView, { PROVIDER_GOOGLE , Marker } from "react-native-maps" ;
-import { View , Text, StyleSheet , StatusBar} from "react-native" ;
+import React, { useState, useEffect } from "react";
+import { MARKERS } from "../DATABASE";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import AppButton from "../components/AppButton";
-import MapViewDirections from 'react-native-maps-directions';
-import * as Location from 'expo-location';
+import MapViewDirections from "react-native-maps-directions";
+import * as Location from "expo-location";
 import Background from "../components/Background";
 
-
-const GOOGLE_MAPS_APIKEY = 'AIzaSyDMOei5uHcZHiLsh-BYhdYYnkIsjr9PGsI';
+const GOOGLE_MAPS_APIKEY = "GOOGLE_MAPS_API_KEY";
 
 //long 35.7212919
 //lat 34.1013648  {latitude :34.098802, longitude: 35.723287 } orining {latitude :34.101935 , longitude: 35.719327 }
 
-// ,    , 
-
+// ,    ,
 
 const Maps = () => {
-    const [ location , setLocation ] = useState({}) ;
-    const [ destination , setDestination ] = useState({});
-    const getLocation = async () => {
-        let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== 'granted') {
-            console.log("not granted")
-            return;
-        }
-        let userLocation = await Location.getCurrentPositionAsync({});
-       
-        setLocation({
-            latitude: userLocation.coords.latitude ,
-            longitude: userLocation.coords.longitude
-        });
+  const [location, setLocation] = useState({});
+  const [destination, setDestination] = useState({});
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      console.log("not granted");
+      return;
     }
-   
-    useEffect(() => {
-        getLocation();
-    }, []);
+    let userLocation = await Location.getCurrentPositionAsync({});
 
-    return (
-       <Background title='Map'>
-       
-              <MapView
-                provider={PROVIDER_GOOGLE}
-                showsUserLocation={true}
-                style={styles.map}
-                followsUserLocation={true}
-                showsMyLocationButton={true}
-                showsTraffic={true}
-                >
-                    <MapViewDirections
-                        destination={destination.latitude && destination}
-                        apikey={GOOGLE_MAPS_APIKEY}
-                        origin={ location.latitude && location }
-                        strokeWidth={6}
-                        strokeColor="red"
-                    />
-                    { MARKERS.map((marker ,index) => (
-                        <Marker key={index} onPress={(e) => destination.latitude ? setDestination({}) : setDestination(e.nativeEvent.coordinate)} coordinate={marker} />
-                    ))}
-                    <Marker onPress={(e) => destination.latitude ? setDestination({}) : setDestination(e.nativeEvent.coordinate)} coordinate={{latitude :34.101935 , longitude: 35.719327 }} />
-            </MapView>
+    setLocation({
+      latitude: userLocation.coords.latitude,
+      longitude: userLocation.coords.longitude,
+    });
+  };
 
-       </Background>
-    );
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return (
+    <Background title="Map">
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        style={styles.map}
+        followsUserLocation={true}
+        showsMyLocationButton={true}
+        showsTraffic={true}
+      >
+        <MapViewDirections
+          destination={destination.latitude && destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+          origin={location.latitude && location}
+          strokeWidth={6}
+          strokeColor="red"
+        />
+        {MARKERS.map((marker, index) => (
+          <Marker
+            key={index}
+            onPress={(e) =>
+              destination.latitude
+                ? setDestination({})
+                : setDestination(e.nativeEvent.coordinate)
+            }
+            coordinate={marker}
+          />
+        ))}
+        <Marker
+          onPress={(e) =>
+            destination.latitude
+              ? setDestination({})
+              : setDestination(e.nativeEvent.coordinate)
+          }
+          coordinate={{ latitude: 34.101935, longitude: 35.719327 }}
+        />
+      </MapView>
+    </Background>
+  );
 };
 
-
 const styles = StyleSheet.create({
-  map:{
-    height: "100%" , 
+  map: {
+    height: "100%",
     width: "100%",
     borderWidth: 3,
-    borderColor: '#00baf2'
-  }
+    borderColor: "#00baf2",
+  },
 });
 
-export default Maps ;
+export default Maps;
